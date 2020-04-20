fce1a21234ce3f7d4372e8ded219fbe30ea998ea
Sławomir <ruudn7@wp.pl>
2020-04-20 21:53:46 +0200

    add necessery files

---
+++ shop/screens/shop/ProductOverviewScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, FlatList, Text, StyleSheet } from 'react-native';
+
+const ProductOverviewScreen = props => {
+    return (
+        <FlatList />
+    );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default ProductOverviewScreen;   
