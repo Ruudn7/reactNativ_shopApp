27af92aec015f7c7d358f370b86469e618566e77
Sławomir <ruudn7@wp.pl>
2020-04-20 23:53:49 +0200

    style product list

--- shop/screens/shop/ProductOverviewScreen.tsx
+++ shop/screens/shop/ProductOverviewScreen.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { View, FlatList, Text, StyleSheet } from 'react-native';
-
+import { FlatList, StyleSheet } from 'react-native';
 import { useSelector } from 'react-redux';
+
+import ProductItem from '../../components/shop/productItem';
+
 
 const ProductOverviewScreen = props => {
     const products = useSelector(state => state.products.availableProducts)
@@ -10,11 +12,21 @@ const ProductOverviewScreen = props => {
             data={products}
             keyExtractor={item => item.id}
             renderItem={
-                itemData => <Text>{itemData.item.title}</Text>
+                itemData => <ProductItem
+                image={itemData.item.imageUrl}
+                title={itemData.item.title}
+                price={itemData.item.price}
+                onViewDetail={() => {}}
+                onAddToCaart={() => {}}
+            />
             }
         />
     );
 };
+
+ProductOverviewScreen.navigationOptions = {
+    headerTitle: 'All products'
+}
 
 const styles = StyleSheet.create({
 
